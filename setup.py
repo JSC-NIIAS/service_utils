1a17f466faa5e6f3f1dd5c814d8e550747332ad8
Sid1057 <ivanov.dale@gmail.com>
2019-01-27 16:25:55 +0300

    setup.py code version changed

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name='service_utils',
-    version='0.1.5b',
+    version='0.1.6',
     description='Python Service Utilities',
     long_description=long_description,
     # long_description_content_type='text/markdown',
